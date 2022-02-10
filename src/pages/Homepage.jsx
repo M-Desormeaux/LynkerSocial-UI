58f534a14eb284427c6bf289adf333f1e2c9684b
Michael Desormeaux <mdesormeaux96@gmail.com>
2022-02-10 15:47:38 -0600

    apihooks

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,16 @@
-export const Homepage = () => {
-  return <></>;
+import { useEffect, useState } from "react";
+import { Lynk } from "../components/Lynk/Lynk";
+import { UserCard } from "../components/UserCard/UserCard";
+import { useGet } from "../Hooks/useGet";
+
+export const HomePage = () => {
+  const { data, isLoaded } = useGet("/api/Post");
+  console.log(data);
+
+  return (
+    <>
+      <UserCard name="Tom" score={9001} />
+      {isLoaded && data.map((post, index) => <Lynk key={index} {...post} />)}
+    </>
+  );
 };
