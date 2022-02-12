e7032c856fda87e6900bc3bd7da43fd690af1bce
Joseph Ellsberry <jellsberry147@gmail.com>
2022-02-12 10:06:28 -0800

    Added Style on CommunityLynk and UserLynk

--- src/components/Lynk/CommunityLynk/CommunityLynk.jsx
+++ src/components/Lynk/CommunityLynk/CommunityLynk.jsx
@@ -1,21 +1,38 @@
+import { ImageWrapper, ShellImage } from "../../Generic.styles";
+import { H2, P } from "../../Typography/Typography.styles";
+import {
+  LynkPadded,
+  LynkPostContent,
+  LynkPostInfo,
+  LynkWrapper,
+} from "../Lynk.styles";
+
 export const CommunityLynk = (props) => {
-  const { name, description, user } = props;
+  const { user, creationDate, description, body } = props;
+  const date = creationDate ? new Date(creationDate) : new Date();
+  const postDate = `${
+    date.getMonth() + 1
+  } | ${date.getDate()} | ${date.getFullYear()}`;
 
   return (
     <>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          padding: "1rem",
-        }}
-      >
-        <div>
-          <div>{name}</div>
-          <div>{description}</div>
-        </div>
-        <div>{user.name}</div>
-      </div>
+      <LynkWrapper>
+        <ImageWrapper>
+          <ShellImage />
+        </ImageWrapper>
+        <LynkPostContent>
+          <LynkPadded>
+            <H2 size="m">{user.name}</H2>
+            <P>{body}</P>
+          </LynkPadded>
+          <LynkPadded>
+            <LynkPostInfo>
+              <P size="s">{description}</P>
+              <P size="s">{postDate}</P>
+            </LynkPostInfo>
+          </LynkPadded>
+        </LynkPostContent>
+      </LynkWrapper>
     </>
   );
 };
